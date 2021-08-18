365473b7855436e76d7c80e7bd2c1f50fa89712f
kimothokamau <kimothokamau@gmail.com>
2021-08-18 09:59:43 -0600

    Add calculator grid structure using reactstrap

---
+++ src/components/Calculator.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+// eslint-disable-next-line react/prefer-stateless-function
+class Calculator extends React.Component {
+  render() {
+    return (
+      <Container className="main-container">
+        <Row>
+          <Col className="answer"> 0 </Col>
+        </Row>
+        <Row>
+          <Col className="item item-gray"> AC </Col>
+          <Col className="item item-gray"> +/- </Col>
+          <Col className="item item-gray"> % </Col>
+          <Col className="item item-orange"> / </Col>
+        </Row>
+
+        <Row>
+          <Col className="item item-gray"> 7 </Col>
+          <Col className="item item-gray"> 8 </Col>
+          <Col className="item item-gray"> 9 </Col>
+          <Col className="item item-orange"> X </Col>
+        </Row>
+
+        <Row>
+          <Col className="item item-gray"> 4 </Col>
+          <Col className="item item-gray"> 5 </Col>
+          <Col className="item item-gray"> 6 </Col>
+          <Col className="item item-orange"> - </Col>
+        </Row>
+
+        <Row>
+          <Col className="item item-gray"> 1 </Col>
+          <Col className="item item-gray"> 2 </Col>
+          <Col className="item item-gray"> 3 </Col>
+          <Col className="item item-orange"> + </Col>
+        </Row>
+
+        <Row>
+          <Col className="item item-double"> 0 </Col>
+          <Col className="item item-gray"> . </Col>
+          <Col className="item item-orange"> = </Col>
+        </Row>
+
+      </Container>
+    );
+  }
+}
+
+export default Calculator;
